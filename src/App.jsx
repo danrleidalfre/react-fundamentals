68b03b8090c07a3ddb2a3127c3d57d122d49cd8e
Danrlei <danrleidalfre@gmail.com>
2023-12-07 18:31:58 -0300

    implement sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import {Header} from "./components/Header.jsx";
 
+import  styles from './App.module.css'
+
 import './global.css'
+import {Sidebar} from "./components/Sidebar.jsx";
 
 export function App() {
-  return (
-    <div>
-      <Header/>
-    </div>
-  )
+    return (
+        <div>
+            <Header />
+
+            <div className={styles.wrapper}>
+                <Sidebar />
+            </div>
+        </div>
+    )
 }
